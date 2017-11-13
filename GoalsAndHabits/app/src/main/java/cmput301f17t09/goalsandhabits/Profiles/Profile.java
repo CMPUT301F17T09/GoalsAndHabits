cc461ca06b688b53117b278a79b2de12c2c96aee
cchiasson <chiasson@ualberta.ca>
2017-11-13 13:21:34 -0700

    added JUnit test for Habit class

--- GoalsAndHabits/app/src/main/java/cmput301f17t09/goalsandhabits/Profiles/Profile.java
+++ GoalsAndHabits/app/src/main/java/cmput301f17t09/goalsandhabits/Profiles/Profile.java
@@ -16,27 +16,53 @@ public class Profile {
     private Long userId;
     private String username;
 
+    /**
+     * Profile constructor used for testing
+     */
+
     public Profile(){
         this.userId = 0L;
     }
 
+    /**
+     * Profile Constructor
+     * @param username
+     */
+
     public Profile(String username){
         this.userId = 0L;
         setUsername(username);
+        //TODO: check for uniqueness of username, assign unique userID
     }
 
+    /**
+     * Gets userID
+     * @return userId
+     */
     public Long getUserId(){
         return this.userId;
     }
 
+    /**
+     * Gets username
+     * @return username
+     */
     public String getUsername(){
         return this.username;
     }
 
+    /**
+     * Sets userID
+     * @param userId
+     */
     public void setUserId(Long userId){
         this.userId = userId;
     }
 
+    /**
+     * Sets username
+     * @param username
+     */
     public void setUsername(String username){
         this.username = username;
     }
