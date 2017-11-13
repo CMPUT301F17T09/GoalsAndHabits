c79e08ba83261ca02935d3b9adcc200c0a6fc358
hidalgok <kenchristopherhidalgo@gmail.com>
2017-11-13 13:11:30 -0700

    made date picker dialog, not implement

---
+++ GoalsAndHabits/app/src/main/java/cmput301f17t09/goalsandhabits/Main_Habits/DatePickerFrag.java
@@ -0,0 +1,34 @@
+package cmput301f17t09.goalsandhabits.Main_Habits;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.widget.DatePicker;
+
+/**
+ * Created by Ken on 13/11/2017.
+ */
+
+public class DatePickerFrag extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        // Do something with the date chosen by the user
+    }
+}
