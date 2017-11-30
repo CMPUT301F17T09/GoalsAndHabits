c49403199a12e45a9112cb5d016d5051bbcfdf3c
hidalgok <kenchristopherhidalgo@gmail.com>
2017-11-29 23:04:25 -0700

    small change to ViewEventActivity so app can compile

--- GoalsAndHabits/app/src/main/java/cmput301f17t09/goalsandhabits/Main_Habits/ViewEventActivity.java
+++ GoalsAndHabits/app/src/main/java/cmput301f17t09/goalsandhabits/Main_Habits/ViewEventActivity.java
@@ -147,9 +147,9 @@ public class ViewEventActivity extends AppCompatActivity implements EditHabitEve
         super.finish();
     }
 
-
+    
     @Override
-    public void onDialogPositiveClick(DialogFragment dialog, String s, String newComment, Date newDate) {
+    public void onDialogPositiveClick(DialogFragment dialog, String s, Date newDate) {
 
     }
 
