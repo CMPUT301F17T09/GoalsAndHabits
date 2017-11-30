25cf3a9ce6ef96ea1311a9452594e1229c79cce4
hidalgok <kenchristopherhidalgo@gmail.com>
2017-11-29 22:55:36 -0700

    added location services and more marker testing

--- GoalsAndHabits/app/src/main/java/cmput301f17t09/goalsandhabits/Maps/MapFiltersActivity.java
+++ GoalsAndHabits/app/src/main/java/cmput301f17t09/goalsandhabits/Maps/MapFiltersActivity.java
@@ -1,6 +1,12 @@
 package cmput301f17t09.goalsandhabits.Maps;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,12 +16,16 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import cmput301f17t09.goalsandhabits.Follow_Activity.FollowActivity;
 import cmput301f17t09.goalsandhabits.Main_Habits.MainActivity;
@@ -26,16 +36,39 @@ import cmput301f17t09.goalsandhabits.R;
 /**
  * This activity allows users to view habit events based on their locations, and can be filtered.
  */
-public class MapFiltersActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MapFiltersActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener {
 
     private MapView map;
     private GoogleMap gmap;
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private FusedLocationProviderClient mFusedLocationClient;
+    Location currentLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTitle("MapFiltersActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_filters);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkLocationPermission()){
+
+            }else{
+                requestPermission();
+            }
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            currentLoc = location;
+                        }
+                    }
+                });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.actionbar);
         toolbar.setTitle("Map");
@@ -99,7 +132,37 @@ public class MapFiltersActivity extends AppCompatActivity implements OnMapReadyC
         return super.onCreateOptionsMenu(menu);
     }
 
+    private Boolean checkLocationPermission(){
+        int result = ContextCompat.checkSelfPermission(MapFiltersActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MapFiltersActivity.this,
+                            "Permission accepted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MapFiltersActivity.this,
+                            "Permission required to continue", Toast.LENGTH_SHORT).show();
+                    requestPermission();
+
+                }
+                break;
+        }
+    }
+
+    //Temporary method for testing marker stuff
     private void clearMap(GoogleMap map) {
         map.clear();
         map.addMarker(new MarkerOptions()
@@ -107,18 +170,25 @@ public class MapFiltersActivity extends AppCompatActivity implements OnMapReadyC
                 .title("Marker"));
     }
 
+    //Temporary method for testing marker stuff
     private void addMarkers(GoogleMap map) {
         map.addMarker(new MarkerOptions()
-                .position(new LatLng(10, 10))
-                .title("Marker"));
+                .position(new LatLng(currentLoc.getLatitude()+0.0005, currentLoc.getLongitude()+0.0005))
+                .title("Super Marker"));
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
         gmap = map;
-        gmap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkLocationPermission()){
+
+            }else{
+                requestPermission();
+            }
+        }
+        gmap.setMyLocationEnabled(true);
+        gmap.setOnMyLocationButtonClickListener(this);
     }
 
     /**
@@ -158,4 +228,26 @@ public class MapFiltersActivity extends AppCompatActivity implements OnMapReadyC
         map.onResume();
     }
 
+    @Override
+    public boolean onMyLocationButtonClick() {
+        //shouldn't be necessary but following method is complaining
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkLocationPermission()){
+
+            }else{
+                requestPermission();
+            }
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            currentLoc = location;
+                        }
+                    }
+                });
+        return false;
+    }
 }
