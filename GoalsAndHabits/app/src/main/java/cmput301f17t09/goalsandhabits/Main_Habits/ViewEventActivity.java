5c282ce1b0fb6975651758b4d6dea23b42a89018
cchiasson <chiasson@ualberta.ca>
2017-12-01 10:41:15 -0700

    making merge-able

--- GoalsAndHabits/app/src/main/java/cmput301f17t09/goalsandhabits/Main_Habits/ViewEventActivity.java
+++ GoalsAndHabits/app/src/main/java/cmput301f17t09/goalsandhabits/Main_Habits/ViewEventActivity.java
@@ -1,36 +1,72 @@
 package cmput301f17t09.goalsandhabits.Main_Habits;
 
+import android.Manifest;
 import android.app.DialogFragment;
+import android.app.usage.UsageEvents;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
+import cmput301f17t09.goalsandhabits.Maps.MapFiltersActivity;
 import cmput301f17t09.goalsandhabits.R;
+
+import static cmput301f17t09.goalsandhabits.Main_Habits.MainActivity.FILENAME;
 
 /**
  * Created by Simone on 2017/11/22.
  */
 
 public class ViewEventActivity extends AppCompatActivity implements EditHabitEventDialog.EditHabitEventDialogListener {
+    private static final int PERMISSION_REQUEST_CODE = 1;
     private HabitEvent event;
     private TextView comment;
+    private TextView statusText;
+    private TextView eventdate;
     private Context context;
     private int position;
     private Toolbar toolbar;
     private boolean deleted = false;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd");
+    private Location currentLoc;
+    private FusedLocationProviderClient mFusedLocationClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_habit_events);
-//        loadFromFile();
 
         Bundle extras = getIntent().getExtras();
         if (extras!=null){
@@ -41,26 +77,25 @@ public class ViewEventActivity extends AppCompatActivity implements EditHabitEve
         }
         if (event==null) finish();
 
-        comment = (TextView) findViewById(R.id.eventComment);
-        //  if (comment == null){return;}
-        //  else{comment.setText(event.getComment());}
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+
+        comment = (TextView) findViewById(R.id.eventComment);
+        comment = (TextView) findViewById(R.id.eventComment);
+        if (comment == null){return;}
+        else{comment.setText(event.getComment());}
+        eventdate = (TextView) findViewById(R.id.eventDate);
+        eventdate.setText(dateFormat.format(event.getDate()));
+        statusText = (TextView) findViewById(R.id.trackPlan);
         toolbar = (Toolbar) findViewById(R.id.actionbar);
         toolbar.setTitle("Habit Event");
         toolbar.setNavigationIcon(R.drawable.ic_close_button);
         setSupportActionBar(toolbar);
 
-//        Button EventsButton = (Button) findViewById(R.id.buttonHabitEvents);
-//        EventsButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i = new Intent(ViewEventActivity.this, HabitHistoryActivity.class);
-//                i.putExtra(HabitHistoryActivity.EXTRA_EVENT_SERIAL, event);
-//                startActivityForResult(i,HabitHistoryActivity.REQUEST_CODE_VIEW_EVENT);
-//            }
-//        });
+        comment.setText(event.getComment());
 
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_view_habit, menu);
@@ -96,24 +131,8 @@ public class ViewEventActivity extends AppCompatActivity implements EditHabitEve
 
 
     public void showEditDialog() {
-        DialogFragment dialog = EditHabitEventDialog.newInstance(event.getComment(),event.getPhotoPath());
+        DialogFragment dialog = EditHabitEventDialog.newInstance(event.getComment(),event.getPhotoPath(),event.getLocation());
         dialog.show(getFragmentManager(), "EditHabitEventDialog");
-    }
-
-
-
-    public void onDialogPositiveClick(DialogFragment dialog, String newComment, String newPhotoPath,
-                                      Location newLocation) {
-        comment.setText(newComment);
-        event.setComment(newComment);
-        event.setPhotoPath(newPhotoPath);
-        //Not updating, will have to make changes to main activity
-    }
-
-
-    public void showEditEventDialog() {
-        //DialogFragment dialog = EditHabitDialog.newInstance(event.getComment(), event.getPhotoPath());
-        //dialog.show(getFragmentManager(), "EditHabitEventDialog");
     }
 
     @Override
@@ -130,12 +149,60 @@ public class ViewEventActivity extends AppCompatActivity implements EditHabitEve
     }
 
     @Override
-    public void onDialogPositiveClick(DialogFragment dialog, String s, Date newDate) {
+    public void onDialogPositiveClick(DialogFragment dialog, String newcomment, Location newloc) {
+        event.setLocation(newloc);
+        event.setComment(newcomment);
+        comment.setText(newcomment);
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
 
     }
 
     @Override
-    public void onDialogNegativeƒClick(DialogFragment dialog) {
+    public Location onLocButtonClick(DialogFragment dialog) {
+        if (checkLocationPermission()){
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                currentLoc = location;
+                            }
+                        }
+                    });
+        }else{requestPermission();}
 
+        return currentLoc;
+    }
+
+    private Boolean checkLocationPermission(){
+        int result = ContextCompat.checkSelfPermission(ViewEventActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(ViewEventActivity.this,
+                            "Permission granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(ViewEventActivity.this,
+                            "Permission required", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
